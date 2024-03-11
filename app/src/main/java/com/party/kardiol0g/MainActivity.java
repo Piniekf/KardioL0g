ceaa75b8a64986d1e1af84cfb811158855f4c9d8
Piniekf <piniekf@gmail.com>
2024-03-11 10:13:24 +0100

    Initial commit

---
+++ app/src/main/java/com/party/kardiol0g/MainActivity.java
@@ -0,0 +1,14 @@
+package com.party.kardiol0g;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
