031c2942d0cd590b861ebe6f8a3af8fd36f4cc53
Piniekf <piniekf@gmail.com>
2024-04-24 14:26:19 +0200

    Zrobiona działająca lista leków.

--- app/src/main/java/com/party/kardiol0g/MedicineFragment.java
+++ app/src/main/java/com/party/kardiol0g/MedicineFragment.java
@@ -1,11 +1,11 @@
 package com.party.kardiol0g;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -22,8 +22,9 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
+
 
 public class MedicineFragment extends Fragment {
 
@@ -42,7 +43,7 @@ public class MedicineFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         View view = inflater.inflate(R.layout.fragment_medicine, container, false);
         listViewMedicines = view.findViewById(R.id.listViewMedicines);
         medicineList = new ArrayList<>();
@@ -54,29 +55,28 @@ public class MedicineFragment extends Fragment {
                 if (listItemView == null) {
                     listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item_medicine, parent, false);
                 }
-
                 TextView medicineNameTextView = listItemView.findViewById(R.id.textMedicineName);
                 TextView medicineDetailsTextView = listItemView.findViewById(R.id.textMedicineDetails);
                 TextView medicineDoseTextView = listItemView.findViewById(R.id.textMedicineDose);
                 TextView medicineQuantityTextView = listItemView.findViewById(R.id.textMedicineQuantity);
                 TextView medicineNoteTextView = listItemView.findViewById(R.id.textMedicineNote);
-                ImageView morningImageView = listItemView.findViewById(R.id.imageMorning);
-                ImageView noonImageView = listItemView.findViewById(R.id.imageNoon);
-                ImageView eveningImageView = listItemView.findViewById(R.id.imageEvening);
+
+                TextView morningTextView = listItemView.findViewById(R.id.textMorning);
+                TextView noonTextView = listItemView.findViewById(R.id.textNoon);
+                TextView eveningTextView = listItemView.findViewById(R.id.textEvening);
 
                 String medicineInfo = getItem(position);
                 String[] medicineInfoArray = medicineInfo.split("\n");
+                Log.d("MedicineFragment", "Medicine Info Array: " + Arrays.toString(medicineInfoArray));
 
                 medicineNameTextView.setText(medicineInfoArray[0]);
                 medicineDetailsTextView.setText(medicineInfoArray[1]);
                 medicineDoseTextView.setText(medicineInfoArray[2]);
                 medicineQuantityTextView.setText(medicineInfoArray[3]);
+                morningTextView.setText(medicineInfoArray[4].contains("Tak") ? "Rano: Tak |" : "Rano: Nie |");
+                noonTextView.setText(medicineInfoArray[5].contains("Tak") ? "Południe: Tak |" : "Południe: Nie |");
+                eveningTextView.setText(medicineInfoArray[6].contains("Tak") ? "Wieczór: Tak" : "Wieczór: Nie");
                 medicineNoteTextView.setText(medicineInfoArray[7]);
-
-                morningImageView.setVisibility(medicineInfoArray[4].equals("true") ? View.VISIBLE : View.GONE);
-                noonImageView.setVisibility(medicineInfoArray[5].equals("true") ? View.VISIBLE : View.GONE);
-                eveningImageView.setVisibility(medicineInfoArray[6].equals("true") ? View.VISIBLE : View.GONE);
-
 
                 return listItemView;
             }
@@ -104,27 +104,28 @@ public class MedicineFragment extends Fragment {
                 public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                     medicineList.clear();
                     for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                        Map<String, Object> medicineData = (Map<String, Object>) snapshot.getValue();
-                        if (medicineData != null) {
-                            String medicineName = (String) medicineData.get("name");
-                            String medicineStrength = (String) medicineData.get("strength");
-                            String dose = (String) medicineData.get("dose");
-                            String quantity = (String) medicineData.get("quantity");
-                            boolean morning = (boolean) medicineData.get("morning");
-                            boolean noon = (boolean) medicineData.get("noon");
-                            boolean evening = (boolean) medicineData.get("evening");
-                            String note = (String) medicineData.get("note");
+                        String medicineName = snapshot.child("name").getValue(String.class);
+                        String medicineStrength = snapshot.child("strength").getValue(String.class);
+                        String dose = snapshot.child("dose").getValue(String.class);
+                        String quantity = snapshot.child("quantity").getValue(String.class);
+                        boolean morning = Boolean.TRUE.equals(snapshot.child("morning").getValue(Boolean.class));
+                        boolean noon = Boolean.TRUE.equals(snapshot.child("noon").getValue(Boolean.class));
+                        boolean evening = Boolean.TRUE.equals(snapshot.child("evening").getValue(Boolean.class));
+                        String note = snapshot.child("note").getValue(String.class);
 
-                            String medicineInfo = "Nazwa: " + medicineName + "\n" +
-                                    "Moc: " + medicineStrength + "\n" +
-                                    "Dawka: " + dose + "\n" +
-                                    "Ilość: " + quantity + "\n" +
-                                    "Rano: " + morning + "\n" +
-                                    "Południe: " + noon + "\n" +
-                                    "Wieczór: " + evening + "\n" +
-                                    "Notatka: " + note + "\n";
-                            medicineList.add(medicineInfo);
-                        }
+                        Log.d("MedicineFragment", "Morning: " + morning);
+                        Log.d("MedicineFragment", "Noon: " + noon);
+                        Log.d("MedicineFragment", "Evening: " + evening);
+
+                        String medicineInfo = "Nazwa: " + medicineName + "\n" +
+                                "Moc: " + medicineStrength + "\n" +
+                                "Dawka: " + dose + "\n" +
+                                "Ilość: " + quantity + "\n" +
+                                "Rano: " + (morning ? "Tak" : "Nie") + "\n" +
+                                "Południe: " + (noon ? "Tak" : "Nie") + "\n" +
+                                "Wieczór: " + (evening ? "Tak" : "Nie") + "\n" +
+                                "Notatka: " + note + "\n";
+                        medicineList.add(medicineInfo);
                     }
                     adapter.notifyDataSetChanged();
                 }
@@ -136,4 +137,5 @@ public class MedicineFragment extends Fragment {
             });
         }
     }
+
 }
