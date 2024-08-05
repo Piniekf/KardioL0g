6da7e58721a2831937952deab01d0d3ebf105744
Piniekf <piniekf@gmail.com>
2024-08-05 13:44:09 +0200

    Poprawione logowanie.

--- app/src/main/java/com/party/kardiol0g/loginregister/LoginActivity.java
+++ app/src/main/java/com/party/kardiol0g/loginregister/LoginActivity.java
@@ -11,11 +11,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -25,14 +22,9 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.party.kardiol0g.MainActivity;
@@ -48,6 +40,7 @@ public class LoginActivity extends AppCompatActivity {
     private GoogleSignInButton googleBtn;
     private GoogleSignInOptions gOptions;
     private GoogleSignInClient gClient;
+    private ActivityResultLauncher<Intent> activityResultLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,164 +59,120 @@ public class LoginActivity extends AppCompatActivity {
         // Sprawdź, czy użytkownik jest już zalogowany przy starcie aplikacji
         FirebaseUser currentUser = auth.getCurrentUser();
         if (currentUser != null) {
-            // Przekieruj bezpośrednio do MainActivity, jeśli użytkownik jest już zalogowany
-            startActivity(new Intent(LoginActivity.this, MainActivity.class));
-            finish(); // Nie pozostawiaj LoginActivity na stosie zadań
+            navigateToAppropriateActivity();
         }
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email = loginEmail.getText().toString();
-                String pass = loginPassword.getText().toString();
+        loginButton.setOnClickListener(v -> {
+            String email = loginEmail.getText().toString();
+            String pass = loginPassword.getText().toString();
 
-                if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-                    if (!pass.isEmpty()) {
-                        loginUser(email, pass);
-                    } else {
-                        loginPassword.setError("Puste pola są niedozwolone");
-                    }
-                } else if (email.isEmpty()) {
-                    loginEmail.setError("Puste pola są niedozwolone");
+            if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                if (!pass.isEmpty()) {
+                    loginUser(email, pass);
                 } else {
-                    loginEmail.setError("Proszę wprowadzic poprawny email");
+                    loginPassword.setError("Puste pola są niedozwolone");
                 }
+            } else if (email.isEmpty()) {
+                loginEmail.setError("Puste pola są niedozwolone");
+            } else {
+                loginEmail.setError("Proszę wprowadzic poprawny email");
             }
         });
 
-        signupRedirectText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
-            }
-        });
+        signupRedirectText.setOnClickListener(v ->
+                startActivity(new Intent(LoginActivity.this, SignUpActivity.class))
+        );
 
-        forgotPassword.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
-                View dialogView = getLayoutInflater().inflate(R.layout.dialog_forgot, null);
-                EditText emailBox = dialogView.findViewById(R.id.emailBox);
+        forgotPassword.setOnClickListener(view -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+            View dialogView = getLayoutInflater().inflate(R.layout.dialog_forgot, null);
+            EditText emailBox = dialogView.findViewById(R.id.emailBox);
 
-                builder.setView(dialogView);
-                AlertDialog dialog = builder.create();
+            builder.setView(dialogView);
+            AlertDialog dialog = builder.create();
 
-                dialogView.findViewById(R.id.btnReset).setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        String userEmail = emailBox.getText().toString();
+            dialogView.findViewById(R.id.btnReset).setOnClickListener(view1 -> {
+                String userEmail = emailBox.getText().toString();
 
-                        if (TextUtils.isEmpty(userEmail) && !Patterns.EMAIL_ADDRESS.matcher(userEmail).matches()){
-                            Toast.makeText(LoginActivity.this, "Wprowadź swój email", Toast.LENGTH_SHORT).show();
-                            return;
-                        }
-                        auth.sendPasswordResetEmail(userEmail).addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                if (task.isSuccessful()){
-                                    Toast.makeText(LoginActivity.this, "Sprawdź swoją pocztę", Toast.LENGTH_SHORT).show();
-                                    dialog.dismiss();
-                                } else {
-                                    Toast.makeText(LoginActivity.this, "Wysyłanie nie powiodło się", Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        });
-                    }
-                });
-                dialogView.findViewById(R.id.btnCancel).setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
+                if (TextUtils.isEmpty(userEmail) || !Patterns.EMAIL_ADDRESS.matcher(userEmail).matches()) {
+                    Toast.makeText(LoginActivity.this, "Wprowadź swój email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                auth.sendPasswordResetEmail(userEmail).addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(LoginActivity.this, "Sprawdź swoją pocztę", Toast.LENGTH_SHORT).show();
                         dialog.dismiss();
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Wysyłanie nie powiodło się", Toast.LENGTH_SHORT).show();
                     }
                 });
-                if (dialog.getWindow() != null){
-                    dialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
-                }
-                dialog.show();
+            });
+
+            dialogView.findViewById(R.id.btnCancel).setOnClickListener(view12 -> dialog.dismiss());
+
+            if (dialog.getWindow() != null) {
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
             }
+            dialog.show();
         });
 
         // Konfiguracja logowania przez Google
         gOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
         gClient = GoogleSignIn.getClient(this, gOptions);
 
-        GoogleSignInAccount gAccount = GoogleSignIn.getLastSignedInAccount(this);
-        if (gAccount != null){
-            finish();
-            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-            startActivity(intent);
-        }
-
         // Rejestracja ActivityResultLauncher dla logowania przez Google
-        ActivityResultLauncher<Intent> activityResultLauncher;
-        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
-                new ActivityResultCallback<ActivityResult>() {
-                    @Override
-                    public void onActivityResult(ActivityResult result) {
-                        if (result.getResultCode() == RESULT_OK){
-                            Intent data = result.getData();
-                            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-                            try {
-                                task.getResult(ApiException.class);
-                                finish();
-                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                                startActivity(intent);
-                            } catch (ApiException e){
-                                Toast.makeText(LoginActivity.this, "Coś poszło nie tak", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    }
-                });
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+            if (result.getResultCode() == RESULT_OK) {
+                Intent data = result.getData();
+                Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                try {
+                    task.getResult(ApiException.class);
+                    navigateToAppropriateActivity();
+                } catch (ApiException e) {
+                    Toast.makeText(LoginActivity.this, "Coś poszło nie tak", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         // Obsługa kliknięcia przycisku logowania przez Google
-        googleBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent signInIntent = gClient.getSignInIntent();
-                activityResultLauncher.launch(signInIntent);
-            }
+        googleBtn.setOnClickListener(view -> {
+            Intent signInIntent = gClient.getSignInIntent();
+            activityResultLauncher.launch(signInIntent);
         });
     }
 
     // Metoda do logowania użytkownika
     private void loginUser(String email, String pass) {
         auth.signInWithEmailAndPassword(email, pass)
-                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                    @Override
-                    public void onSuccess(AuthResult authResult) {
-                        // Pobierz ID zalogowanego użytkownika
-                        String userId = auth.getCurrentUser().getUid();
-
-                        // Pobierz referencję do użytkownika w bazie danych
-                        DatabaseReference userRef = FirebaseDatabase.getInstance().getReference().child("Users").child(userId);
-
-                        // Odczytaj wartość czyLekarz
-                        userRef.child("czyLekarz").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-                            @Override
-                            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                                if (task.isSuccessful()) {
-                                    Boolean isDoctor = task.getResult().getValue(Boolean.class);
-                                    if (isDoctor != null && isDoctor) {
-                                        // Jeśli użytkownik jest lekarzem, przejdź do MainActivity_Doctor
-                                        startActivity(new Intent(LoginActivity.this, MainActivity_Doctor.class));
-                                    } else {
-                                        // Jeśli użytkownik nie jest lekarzem, przejdź do MainActivity
-                                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
-                                    }
-                                    finish(); // Zakończ LoginActivity
-                                } else {
-                                    // Obsłuż błąd odczytu z bazy danych
-                                    Toast.makeText(LoginActivity.this, "Nie udało się pobrać danych użytkownika", Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        });
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(LoginActivity.this, "Logowanie nie powiodło się", Toast.LENGTH_SHORT).show();
-                    }
-                });
+                .addOnSuccessListener(authResult -> {
+                    // Użytkownik zalogowany - sprawdź jego rolę
+                    navigateToAppropriateActivity();
+                })
+                .addOnFailureListener(e ->
+                        Toast.makeText(LoginActivity.this, "Logowanie nie powiodło się", Toast.LENGTH_SHORT).show()
+                );
     }
 
+    private void navigateToAppropriateActivity() {
+        // Pobierz ID zalogowanego użytkownika
+        String userId = auth.getCurrentUser().getUid();
+        DatabaseReference userRef = FirebaseDatabase.getInstance().getReference().child("Users").child(userId);
+
+        userRef.child("czyLekarz").get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                Boolean isDoctor = task.getResult().getValue(Boolean.class);
+                if (isDoctor != null && isDoctor) {
+                    // Jeśli użytkownik jest lekarzem, przejdź do MainActivity_Doctor
+                    startActivity(new Intent(LoginActivity.this, MainActivity_Doctor.class));
+                } else {
+                    // Jeśli użytkownik nie jest lekarzem, przejdź do MainActivity
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                }
+                finish(); // Zakończ LoginActivity
+            } else {
+                // Obsłuż błąd odczytu z bazy danych
+                Toast.makeText(LoginActivity.this, "Nie udało się pobrać danych użytkownika", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
